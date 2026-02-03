8b37d261f38278af1ffab95a8498e94daf32b785
MatheusMendes <matheusgbe.mendes@outlook.com>
2026-02-02 22:14:50 -0300

    feat: Routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\PlanoController;
+use App\Http\Controllers\AssinaturaController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/planos', [PlanoController::class, 'index'])
+    ->name('planos.index');
+
+Route::post('/assinar', [AssinaturaController::class, 'assinar'])
+    ->middleware('auth')
+    ->name('assinaturas.assinar');
+
+Route::post('/cancelar-assinatura', [AssinaturaController::class, 'cancelar'])
+    ->middleware('auth')
+    ->name('assinaturas.cancelar');
