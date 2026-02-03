33e0c1a8f717436a1a2a09ec45e3d33282816431
MatheusMendes <matheusgbe.mendes@outlook.com>
2026-02-02 22:14:15 -0300

    feat: Models

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,15 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function assinatura()
+    {
+        return $this->hasOne(Assinatura::class, 'usuario_id');
+    }
+
+    public function possuiAssinaturaAtiva()
+    {
+        return $this->assinatura && 
+               $this->assinatura->status === 'ativa';
+    }
 }
